e6b4114d71f148b4ca206754f1a8d740f121b2d3
Gonzalo Torres-Quevedo Acquaroni <torres1991g@gmail.com>
2018-10-27 20:23:01 +0200

    Add files via upload

---
+++ App_Data/UserData.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [UserName] NCHAR(20) NULL, 
+    [Password] NCHAR(20) NULL, 
+    [Email] NCHAR(50) NULL, 
+    [Country] NCHAR(15) NULL
+)
